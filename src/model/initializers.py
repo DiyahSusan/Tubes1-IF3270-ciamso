c3d8fa0fb1d4fa6ca0dfe2e9636ef17b8414e069
Diyah Susan <diyahnugrahani04@gmail.com>
2026-03-10 21:20:42 +0700

    feat : feat initializer

--- src/model/initializers.py
+++ src/model/initializers.py
@@ -3,11 +3,23 @@ import numpy as np
 # initializers sesuai spek
 
 def zero_init(shape): 
-    pass
-def uniform_init(shape, low, high, seed): 
-    pass
-def normal_init(shape, mean, variance, seed): 
-    pass
+    # isi dengan nol
+    return np.zeros(shape)
+
+def uniform_init(shape, low, high, seed=None): 
+    # random dengan distribusi uniform
+    if seed is not None:
+        np.random.seed(seed)
+    return np.random.uniform(low, high, size=shape)
+
+def normal_init(shape, mean, variance, seed=None): 
+    # random dengan distribusi normal
+    if seed is not None:
+        np.random.seed(seed)
+    
+    # standar deviasi
+    std_dev = np.sqrt(variance)
+    return np.random.normal(mean, std_dev, size=shape)
 
 # bonus kalo mau
 def xavier_init(shape): 
