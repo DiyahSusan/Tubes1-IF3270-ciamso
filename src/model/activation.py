8cca672c832f7041b8383162012c9ddbb9c1ed42
Diyah Susan <diyahnugrahani04@gmail.com>
2026-03-11 18:21:03 +0700

    feat : add activation layer

--- src/model/activation.py
+++ src/model/activation.py
@@ -2,6 +2,7 @@ from src.model.layers import Layer
 
 class ActivationLayer(Layer):
     #  base class untuk aktivasi, tinggal ganti fungsi matematika di dalamnya
+    # ini dipake buat yang non linear
 
     def __init__(self, activation, activation_prime):
         super().__init__()
@@ -9,8 +10,10 @@ class ActivationLayer(Layer):
         self.activation_prime = activation_prime
 
     def forward(self, input_data):
-        pass
+        self.input = input_data # simpan untuk backward pass 
+        self.output = self.activation(self.input) # terapkan fungsi aktivasi 
+        return self.output
 
     def backward(self, output_error, learning_rate):
-        pass
+        return self.activation_prime(self.input) * output_error
     
