95eefe419743f48d815f4013f1c2e4c6825a0b5d
Diyah Susan <diyahnugrahani04@gmail.com>
2026-03-15 23:26:40 +0700

    refactor : ubah fungsi fungsi jadi kelas

--- src/model/functions.py
+++ src/model/functions.py
@@ -4,67 +4,75 @@ import numpy as np
 # TODO : implementasikan sesuai kebutuhan
 
 # fungsi aktivasi dan turunan pertamanya
-def linear(x):
-    return x
+class ActivationFunctions:
+    def __init__(self):
+        pass
 
-def linear_prime(x):
-    return 1
+    def linear(self, x):
+        return x
 
-def relu(x):
-    return np.maximum(0, x)
+    def linear_prime(self, x):
+        return 1
 
-def relu_prime(x):
-    return (x > 0).astype(float)
+    def relu(self, x):
+        return np.maximum(0, x)
 
-def sigmoid(x):
-    return 1/(1+ np.exp(-x))
+    def relu_prime(self, x):
+        return (x > 0).astype(float)
 
-def sigmoid_prime(x):
-    return sigmoid(x) * (1 - sigmoid(x))
+    def sigmoid(self, x):
+        return 1/(1+ np.exp(-x))
 
-def hyperbolic_tangent(x):
-    return np.tanh(x)
+    def sigmoid_prime(self, x):
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
 
-def hyperbolic_tangent_prime(x):
-    return 1 - np.tanh(x)**2
+    def hyperbolic_tangent(self, x):
+        return np.tanh(x)
 
-def softmax(x):
-    x = np.array(x)
-    x_shifted = x - np.max(x)
-    exp_x = np.exp(x_shifted)
-    return exp_x/np.sum(exp_x)
+    def hyperbolic_tangent_prime(self, x):
+        return 1 - np.tanh(x)**2
 
-def softmax_prime(x):
-    s = softmax(x).reshape(-1,1)
-    return np.diagflat(s) - np.dot(s,s.T)
+    def softmax(self, x):
+        x = np.array(x)
+        x_shifted = x - np.max(x)
+        exp_x = np.exp(x_shifted)
+        return exp_x/np.sum(exp_x)
+
+    def softmax_prime(self, x):
+        s = self.softmax(x).reshape(-1,1)
+        return np.diagflat(s) - np.dot(s,s.T)
 
 # loss function
-def mse(y_true, y_pred): 
-    return np.mean(np.power(y_true - y_pred, 2))
-
-def mse_prime(y_true, y_pred): 
-    return 2 * (y_pred - y_true) / y_true.size
-
-def binary_cross_entropy(y_true, y_pred): 
-    epsilon = 1e-15
-    y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
-    return -np.mean(y_true * np.log(y_pred) + (1-y_true) * np.log(1-y_pred))
-
-def bce_prime(y_true, y_pred): 
-    epsilon = 1e-15
-    y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
-    n = y_true.size
+class LossFunctions:
+    def __init__(self):        
+        pass    
     
-    return -(y_true / y_pred - (1 - y_true) / (1 - y_pred)) / n
+    def mse(self, y_true, y_pred): 
+        return np.mean(np.power(y_true - y_pred, 2))
 
-def categorical_cross_entropy(y_true, y_pred): 
-    n = y_true.shape[0]
-    epsilon = 1e-15
-    y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
-    return -np.sum(y_true * np.log(y_pred)) / n
-            
-def cce_prime(y_true, y_pred): 
-    n = y_true.shape[0]
-    epsilon = 1e-15
-    y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
-    return -(y_true / y_pred)/n
+    def mse_prime(self, y_true, y_pred): 
+        return 2 * (y_pred - y_true) / y_true.size
+
+    def binary_cross_entropy(self, y_true, y_pred): 
+        epsilon = 1e-15
+        y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
+        return -np.mean(y_true * np.log(y_pred) + (1-y_true) * np.log(1-y_pred))
+
+    def bce_prime(self, y_true, y_pred): 
+        epsilon = 1e-15
+        y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
+        n = y_true.size
+        
+        return -(y_true / y_pred - (1 - y_true) / (1 - y_pred)) / n
+
+    def categorical_cross_entropy(self, y_true, y_pred): 
+        n = y_true.shape[0]
+        epsilon = 1e-15
+        y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
+        return -np.sum(y_true * np.log(y_pred)) / n
+                
+    def cce_prime(self, y_true, y_pred): 
+        n = y_true.shape[0]
+        epsilon = 1e-15
+        y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
+        return -(y_true / y_pred)/n
