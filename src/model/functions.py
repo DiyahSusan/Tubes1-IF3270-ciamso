fix: fixing activation, softmax and mse bce cce, and verbose print

src/model/functions.py
@@ -12,7 +12,7 @@ class ActivationFunctions:
         return x
 
     def linear_prime(self, x):
-        return 1
+        return np.ones_like(x)
 
     def relu(self, x):
         return np.maximum(0, x)
@@ -39,8 +39,15 @@ class ActivationFunctions:
         return exp_x / np.sum(exp_x, axis=1, keepdims=True)
 
     def softmax_prime(self, x):
-        # gradient dari loss lgsg diterusin ke next dense layer
-        return np.ones_like(x) 
+        s = self.softmax(x)
+        batch_jacobian = []
+
+        for row in s:
+            row = row.reshape(-1, 1)
+            jacobian = np.diagflat(row) - np.dot(row, row.T)
+            batch_jacobian.append(jacobian)
+
+        return np.array(batch_jacobian)
 
 # loss function
 class LossFunctions:
@@ -51,7 +58,7 @@ class LossFunctions:
         return np.mean(np.power(y_true - y_pred, 2))
 
     def mse_prime(self, y_true, y_pred): 
-        return 2 * (y_pred - y_true) / y_true.size
+        return 2 * (y_pred - y_true)
 
     def binary_cross_entropy(self, y_true, y_pred): 
         epsilon = 1e-15
@@ -63,7 +70,7 @@ class LossFunctions:
         y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
         n = y_true.size
         
-        return -(y_true / y_pred - (1 - y_true) / (1 - y_pred)) / n
+        return -(y_true / y_pred - (1 - y_true) / (1 - y_pred))
 
     def categorical_cross_entropy(self, y_true, y_pred): 
         n = y_true.shape[0]
@@ -75,4 +82,4 @@ class LossFunctions:
         n = y_true.shape[0]
         epsilon = 1e-15
         y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
-        return -(y_true / y_pred)/n
+        return -(y_true / y_pred)